f32175f705e0b2367e69b289e3ef5c9a29ea545b
Vaidehi_Padmasenan <Vaidehisenan@gmail.com>
2016-02-22 16:00:36 +0530

    Adding new methods

--- selenium/src/main/java/com/automation/framework/web/WebCommand.java
+++ selenium/src/main/java/com/automation/framework/web/WebCommand.java
@@ -1,6 +1,14 @@
 package com.automation.framework.web;
 
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -48,7 +56,17 @@ public class WebCommand {
 	}
 
 	public void click(PageElement pageElement){
-		getWebElement(pageElement).click();
+		try{
+			getWebElement(pageElement).click();
+		}catch(StaleElementReferenceException staleException){
+			getWebElement(pageElement).click();
+		}
+		catch(ElementNotVisibleException elemNotVisibleExc){
+			Assert.fail(pageElement.getName()+" was not visible to click");
+		}		
+		catch(TimeoutException timeoutExc){			
+			//Dont do anything for timeout exception. Just continue with remaining test
+		}
 	}
 
 	public void type(PageElement pageElement,String value){
@@ -73,12 +91,66 @@ public class WebCommand {
 		WebElement webElement=getWebElement(pageElement);
 		driver.switchTo().frame(webElement);
 	}
-	
+
 	public String getText(PageElement pageElement) {
 		String text="";
-		WebElement webElement=getWebElement(pageElement);
-		text=webElement.getText();
-		return text;
+		try{
+			text=getWebElement(pageElement).getText();	
+		}catch (Exception e) {
+			Assert.fail("Couldn't get text for locator"+pageElement);
+		}	
+		return text;	
 	}
-	
+
+	public  WebElement scrollToElementAndClick(PageElement pageElement) throws Exception{
+		WebElement element = null;
+		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+		try{
+			element = getWebElement(pageElement);
+			int y = element.getLocation().getY();
+			executor.executeScript("window.scrollTo(0," + (y-20) + ");");
+			executor.executeScript("arguments[0].click();", element);
+		}
+		catch(Exception e){
+		}
+		return element;
+	}
+	public void waitForElementToVisible(PageElement pageElement,long interval) throws Exception {
+		long counter = 0;
+		while(isElementDisplayed(pageElement)){
+			counter++;
+			if(!isElementDisplayed(pageElement) || counter == interval)
+				break;
+		}
+		Thread.sleep(interval*100);
+	}
+	public void waitFor(long milliseconds){   
+		try{
+			Thread.sleep(milliseconds);
+		}catch(Exception e){
+			System.out.println("Could not wait for "+milliseconds/1000+" Seconds!!");              
+		}
+	}
+	public void scrollTo(int x,int y){
+		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		System.out.println("window.scrollTo("+x +"," +y+");");
+		try{
+			executor.executeScript("window.scrollTo("+x +"," +y+");");
+		}catch(Exception e){
+
+		}
+	}
+	public boolean isElementDisplayed(PageElement pageElement) throws Exception{
+		try{
+			driver.manage().timeouts().implicitlyWait(0, TimeUnit.MILLISECONDS);
+			WebElement element = getWebElement(pageElement);
+			driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+			if (element.isDisplayed());
+			return true;
+		}
+		catch(Exception e){
+			return false;
+		}
+	}
 }
